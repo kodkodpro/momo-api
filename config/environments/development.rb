04ac2a03ae865c551e115d9e6a39f929c5a8b2e5
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2026-03-30 12:16:57 +0100

    Allow all hosts in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Allow any host
+  config.hosts.clear
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
